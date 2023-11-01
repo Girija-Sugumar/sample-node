25ffa5fdfaec7c237863101d57eb60f057b86c28
Girija-Sugumar <133954729+Girija-Sugumar@users.noreply.github.com>
2023-11-01 11:11:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello, Practising to push docker image to ecr!')
 })
 
+app.get('/fun', (req, res) => {
+  res.send('Application deployed')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
